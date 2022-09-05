f63ae83b53c4e0080079dea3394a23025a392ce1
igorazuos <azuosigor@gmail.com>
2022-09-05 16:05:55 -0300

    atualização exercicios a011

---
+++ exercicio1/script.js
@@ -0,0 +1,21 @@
+const arrayScolao = []
+const fruta1 = {
+    nome: "banana",
+    preço: 2,
+    quantidade: 50,
+    disponivel: true,
+}
+const fruta2 = {
+    nome: "coco",
+    preço: 5,
+    quantidade: 25,
+    disponivel: true,
+}
+const fruta3 = {
+    nome: "mamão",
+    preço: 7,
+    quantidade: 0,
+    disponivel: false,
+}
+arrayScolao.push(fruta1, fruta2, fruta3)
+console.log(arrayScolao)
